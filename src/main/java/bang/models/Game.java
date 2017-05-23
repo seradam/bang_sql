b9b8267366cf09e05341a23f096c9e3df90d5ddd
seradam <seradam89@gmail.com>
2017-05-23 14:03:42 +0200

    role getter

--- src/main/java/bang/models/Game.java
+++ src/main/java/bang/models/Game.java
@@ -29,9 +29,24 @@ public class Game {
         return deck;
     }
 
+    public ArrayList<Role> getRoles() throws SQLException {
+        TableFactory tf = new TableFactory();
+        String roleGetterQuery = "SELECT * FROM role";
+        ArrayList<ArrayList<String>> roleNestedList = tf.getData(roleGetterQuery);
+        ArrayList<Role> roles = new ArrayList<Role>();
+        for (int i = 0; i<roleNestedList.size(); i++){
+            int quantityCounter = Integer.parseInt(roleNestedList.get(i).get(3));
+            while (quantityCounter > 0){
+                roles.add(new Role(roleNestedList.get(i).get(1), roleNestedList.get(i).get(2)));
+                quantityCounter--;
+            }
+        }
+        return roles;
+    }
+
     public static void main(String[] args) throws SQLException {
         Game g = new Game();
-        ArrayList<Card> sg = g.buildDeck();
+        ArrayList<Role> sg = g.getRoles();
         for (int i = 0; i<sg.size(); i++){
             System.out.println(sg.get(i).name);
         }
